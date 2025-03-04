d703548c85d5725ff16a182b98e10070aa814ba1
Menghua Li <menghua.li@westjet.com>
2025-03-04 18:50:04 -0500

    init + naming-server

---
+++ naming-server/src/test/java/aloha/spring/microservices/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package aloha.spring.microservices.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+naming-server:9001
