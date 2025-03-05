85ec42cd05a10091876e96af9abe053fd95df463
Menghua Li <menghua.li@westjet.com>
2025-03-04 19:30:11 -0500

    config-server + flight-booking-service

---
+++ config-server/src/test/java/aloha/spring/microservices/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package aloha.spring.microservices.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ flight-booking-service/src/test/java/aloha/spring/microservices/flight_booking_service/FlightBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aloha.spring.microservices.flight_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
