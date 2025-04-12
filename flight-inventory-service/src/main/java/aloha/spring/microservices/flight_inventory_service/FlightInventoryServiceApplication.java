e31aabedd6b9f1f36c6a014217be8b59b6257134
Menghua Li <menghua.li@westjet.com>
2025-04-12 15:39:04 -0400

    Added flight-inventory-service

---
+++ flight-inventory-service/src/main/java/aloha/spring/microservices/flight_inventory_service/FlightInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package aloha.spring.microservices.flight_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ flight-inventory-service/src/test/java/aloha/spring/microservices/flight_inventory_service/FlightInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aloha.spring.microservices.flight_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
